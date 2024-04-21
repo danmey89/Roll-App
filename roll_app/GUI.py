714fb805f3f196619504654e904054066ea592c4
DanMey <danmey@gmx.de>
2024-04-21 22:40:47 +0200

    change turn order & add clear buttons

--- roll_app/GUI.py
+++ roll_app/GUI.py
@@ -10,11 +10,13 @@ print(keyes)
 
 label_sk = sg.Text("Skills:")
 label_to = sg.Text("Turn Order:")
+label_nt = sg.Text("Enter new turn:")
 
-add_turn = sg.InputText(key="turn", do_not_clear=False, size=30)
+add_turn = sg.InputText(tooltip="Name", key="turn", do_not_clear=False, size=20)
+add_num = sg.InputText(tooltip="Number", key="num_t", do_not_clear=False, size=5)
 
 skill_list = sg.Listbox(values=keyes, key="skill", enable_events=True, size=(20, len(keyes)))
-results_list = sg.Listbox(values=results, key="result", size=(30, len(data)))
+results_list = sg.Listbox(values=results, key="result", size=(30, len(data)*2))
 turn_order = sg.Listbox(values=order, key="order", enable_events=True, size=(30, 10))
 
 roll_button = sg.Button("Roll")
@@ -22,16 +24,19 @@ exit_button = sg.Button("Exit")
 add_button = sg.Button("Add Turn")
 next_button = sg.Button("Next")
 remove_button = sg.Button("Remove")
+clear_t_button = sg.Button("Clear", key="Clear_t")
+clear_r_button = sg.Button("Clear", key="Clear_r")
 
 col_1 = sg.Column([
     [label_sk],
-    [skill_list, roll_button, results_list]
+    [skill_list, sg.Column([[roll_button, clear_r_button], [results_list]])]
 ])
 
 col_2 = sg.Column([
     [label_to],
-    [turn_order, next_button, remove_button],
-    [add_turn, add_button],
+    [turn_order, sg.Column([[next_button], [remove_button], [clear_t_button]])],
+    [label_nt],
+    [add_turn, add_num, add_button],
 ])
 
 layout = [[col_1, sg.VerticalSeparator(), col_2]]
@@ -45,7 +50,7 @@ while True:
     match event:
         case "Roll":
             try:
-                results.clear()
+                results.append(values["skill"])
                 for player in data:
                     results.append(roll(values["skill"][0], player))
                 window["result"].update(values=results)
@@ -55,7 +60,8 @@ while True:
         case "Add Turn":
             try:
                 turn = values["turn"]
-                order = get_order(turn, order)
+                number = values["num_t"]
+                order = get_order(number, turn, order)
                 window["order"].update(values=order)
             except IndexError:
                 continue
@@ -75,6 +81,14 @@ while True:
                 window["order"].update(values=order)
             except IndexError:
                 continue
+
+        case "Clear_t":
+            order.clear()
+            window["order"].update(values=order)
+
+        case "Clear_r":
+            results.clear()
+            window["result"].update(values=results)
 
         case "Exit" | sg.WIN_CLOSED:
             break
--- roll_app/functions.py
+++ roll_app/functions.py
@@ -33,9 +33,9 @@ def get_data(filepath=FILEPATH):
     return data
 
 
-def get_order(set_l, order_l):
-    new = [int(v) if v.isnumeric() else v for v in set_l.split()]
-    new = [v.capitalize() if type(v) is str else v for v in new]
+def get_order(numb, name, order_l):
+    numb = int(numb)
+    new = numb, name
     order_l.append(new)
-    order_l = sorted(order_l, key=lambda pair: pair[1], reverse=True)
+    order_l = sorted(order_l, key=lambda pair: pair[0], reverse=True)
     return order_l
